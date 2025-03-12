bda4ae26ede15f59cc235a809b437be87492b3b6
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-03-12 16:43:02 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM ubuntu:22.04
+
+COPY . /app
+WORKDIR /app
+
+RUN apt update && apt install -y bash
+RUN apt install -y build-essential 
+RUN apt install -y libncurses5 libstdc++6 libc++1
+RUN apt install -y python3-pip
+
+RUN tar zxvf neuronpilot-6.0.5_x86_64.tar.gz -C ./
+RUN bash -c "./neuronpilot-6.0.5/neuron_sdk/host/bin/ncc-tflite --version"
+RUN bash -c "pip install -r requirements.txt"
+
+EXPOSE 80
+
+CMD ["python3", "app.py"]
+#CMD ["bash"]
--- app.py
+++ app.py
@@ -1,32 +1,58 @@
-import os
-
+import os, datetime, random, subprocess
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploads'
 
+def random_seed():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    random_number = random.randint(1000, 9999)
+    return f"{timestamp}_{random_number}"
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'tflite'}
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    html = """ 
+    <!doctype html>
+    <title>NeuronPilot Converter</title>
+    <h1>Upload your .tflite model</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
+    if request.method == 'POST':
+        print(request.url)
+        if 'file' not in request.files:
+            html.replace('<h1>Upload your .tflite model</h1>', '<h1>.tflite file not found.</h1>')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            html.replace('<h1>Upload your .tflite model</h1>', '<h1>.tflite file not found.</h1>')
+            return redirect(request.url)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            seed = random_seed()
+            print(f'Generate a upload instance:{seed}')
+            os.makedirs(f"{app.config['UPLOAD_FOLDER']}/{seed}", exist_ok=True)
+            file_path = os.path.join(f"{app.config['UPLOAD_FOLDER']}/{seed}", filename)
+            file.save(file_path)
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+            result = subprocess.run(f"./neuronpilot-6.0.5/neuron_sdk/host/bin/ncc-tflite --arch=mdla3.0 --relax-fp32 {file_path}", shell=True, capture_output=True, text=True)
+            print(f"Return code: {result.returncode}")
+            print(f"Output: {result.stdout}")
+            print(f"Error: {result.stderr}")
 
+            return send_from_directory(f"{app.config['UPLOAD_FOLDER']}/{seed}", filename)
+    return html
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port=80)
+
+
+
